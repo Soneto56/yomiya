0b69a0f19ea06a329e6f33d6d5beb4bd6ce42b34
SOL2123 <1352906993@qq.com>
2025-06-27 23:30:46 +0800

    111

--- demo/src/main/java/com/example/demo/controller/StudentController.java
+++ demo/src/main/java/com/example/demo/controller/StudentController.java
@@ -1,4 +1,4 @@
-package com.example.demo.controller;
+package                                                                                  com.example.demo.controller;
 
 import com.example.demo.entity.Student;
 import com.example.demo.service.StudentService;
