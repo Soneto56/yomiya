f828e0d2534b3ad951c7a038f7c38b0288e37495
SOL2123 <1352906993@qq.com>
2025-06-29 21:43:23 +0800

    Delete demo directory

--- demo/src/main/java/com/example/demo/repository/MajorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Major;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MajorRepository extends JpaRepository<Major, Integer> {
-}
--- demo/src/main/java/com/example/demo/repository/TeacherRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Teacher;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeacherRepository extends JpaRepository<Teacher, String> {
-}
